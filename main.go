85ada7837f2de3a21a2c0eb621d4850f3073e68c
binchoi <binchoi@u.yale-nus.edu.sg>
2023-12-29 18:53:14 +0900

    Add oauth2

--- main.go
+++ main.go
@@ -3,22 +3,66 @@ package main
 import (
 	"context"
 	"errors"
-	"github.com/gin-gonic/gin"
 	"log"
 	"net/http"
 	"os"
 	"os/signal"
 	"syscall"
 	"time"
+
+	"github.com/gin-gonic/gin"
+	"github.com/go-oauth2/oauth2/v4/manage"
+	"github.com/go-oauth2/oauth2/v4/models"
+	"github.com/go-oauth2/oauth2/v4/server"
+	"github.com/go-oauth2/oauth2/v4/store"
 )
 
 func main() {
+	// Initialize the OAuth2 manager
+	manager := manage.NewDefaultManager()
+	manager.MustTokenStorage(store.NewMemoryTokenStore())
+
+	// Client memory store
+	clientStore := store.NewClientStore()
+	clientStore.Set("000000", &models.Client{
+		ID:     "000000",
+		Secret: "999999",
+		Domain: "http://localhost",
+	})
+	manager.MapClientStorage(clientStore)
+
+	// Initialize the OAuth2 server
+	oauthServer := server.NewDefaultServer(manager)
+	oauthServer.SetAllowGetAccessRequest(true)
+	oauthServer.SetClientInfoHandler(server.ClientFormHandler)
+
+	// Create a Gin router
 	router := gin.Default()
 
+	// Define OAuth2 routes
+	router.GET("/authorize", func(c *gin.Context) {
+		err := oauthServer.HandleAuthorizeRequest(c.Writer, c.Request)
+		if err != nil {
+			log.Printf("Authorize error: %s\n", err.Error())
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+	})
+
+	router.POST("/token", func(c *gin.Context) {
+		if err := oauthServer.HandleTokenRequest(c.Writer, c.Request); err != nil {
+			log.Printf("Token error: %s\n", err.Error())
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+	})
+
+	// Your existing routes
 	router.GET("/ping", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{"message": "pong"})
 	})
 
+	// Set up the server with the Gin router as handler
 	srv := &http.Server{
 		Addr:    ":8080",
 		Handler: router,
@@ -31,19 +75,12 @@ func main() {
 		}
 	}()
 
-	// Wait for interrupt signal to gracefully shut down the server
+	// Graceful shutdown setup
 	quit := make(chan os.Signal, 1)
-
-	// Kill (no param) like kill -SIGINT
-	// Kill -2 is like Ctrl+C
-	// SIGTERM is a termination signal
 	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
-
 	<-quit
 	log.Println("Shutting down server...")
 
-	// The context is used to inform the server it has 5 seconds to finish
-	// the operations it is working on
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 
